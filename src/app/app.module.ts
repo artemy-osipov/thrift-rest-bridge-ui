5c63431753e9463ce37823a11ce41182503c6e66
artemy-osipov <osipov.artemy@gmail.com>
2020-05-05 17:00:58 +0300

    proxy layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from 'app/app-routing.module';
 import { FontModule } from 'app/font.module';
-import { MaterialModule } from 'app/material-module';
+import { MaterialModule } from 'app/material.module';
 
 import { AppComponent } from 'app/app.component';
 import { ServiceListComponent } from 'app/service-list/service-list.component';
+import { ServiceProxyComponent } from 'app/service-proxy/service-proxy.component';
 import { HeaderComponent } from 'app/shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ServiceListComponent
+    ServiceListComponent,
+    ServiceProxyComponent
   ],
   imports: [
     BrowserAnimationsModule,
---
+++ src/app/service-proxy/service-proxy.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-proxy',
+  templateUrl: './service-proxy.component.html',
+  styleUrls: ['./service-proxy.component.css']
+})
+export class ServiceProxyComponent { }
