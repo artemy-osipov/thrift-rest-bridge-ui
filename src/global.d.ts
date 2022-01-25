280a80a13aa8a5f01b21491c7db4a771deca0da7
artemy-osipov <osipov.artemy@gmail.com>
2022-01-25 17:25:00 +0300

    deps: update

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,8 @@
 /// <reference types="@sveltejs/kit" />
 
+declare module 'svelte-icons/fa/FaAngleDown.svelte'
+declare module 'svelte-icons/fa/FaSearch.svelte'
+
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 declare module 'svelte-jsoneditor' {
   export const JSONEditor: any
